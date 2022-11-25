ea91cfcb8a9165ed54e17c29b0b1dbe6b776c76d
kimsungyeul <ksyeul92@gmail.com>
2022-11-25 10:42:48 +0000

    Fb_mmap_bmp

---
+++ multi_day3/framebufferShape.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/ioctl.h>
+#include <sys/types.h>
+#include <linux/fb.h> 
+
+#define  FB                      "/dev/fb0"
+#define  NO_OF_COLOR   2
+
+typedef unsigned char ubyte;
+ 
+unsigned short makepixel(ubyte r, ubyte g, ubyte b) {
+	return (unsigned short)( ((r>>3)<<11) | ((g>>2)<<5) | (b>>3) );
+}
+  
+int main(int argc, char** argv)
+{
+	int fd, status, offset,i,j;
+	unsigned short pixel;
+	unsigned char color[NO_OF_COLOR];
+           
+	struct fb_var_screeninfo fbvar;
+	struct fb_fix_screeninfo fbfix;
+
+	fd = open(FB, O_RDWR);
+	if(fd < 0) {
+	printf("Error fbdev open\n");
+		return -1;
+	}
+
+	status = ioctl(fd, FBIOGET_FSCREENINFO, &fbfix);
+	status = ioctl(fd, FBIOGET_VSCREENINFO, &fbvar);
+
+	printf("x-resolution : %d\n", fbvar.xres);
+	printf("y-resolution : %d\n", fbvar.yres);
+	printf("x-resolution(virtual) : %d\n", fbvar.xres_virtual);
+	printf("y-resolution(virtual) : %d\n", fbvar.yres_virtual);
+	printf("bpp : %d\n", fbvar.bits_per_pixel);
+	printf("length of frame buffer memory : %d\n", fbfix.smem_len);
+
+	fbvar.bits_per_pixel = 16;
+	  
+	for(j=0;j<fbvar.yres_virtual;j++) {
+		for(i=0;i<fbvar.xres_virtual;i++){
+			offset = (i + j*fbvar.xres_virtual)*NO_OF_COLOR;
+			lseek(fd, offset, SEEK_SET);
+			pixel = makepixel(0, 0, 255);
+			write(fd, &pixel,NO_OF_COLOR);
+		}
+	}
+
+	printf("ERROR fbdev lseek \n");
+	
+	close(fd);
+	return 0;
+}
---
+++ multi_day3/miniproject_mmap.c
@@ -0,0 +1,80 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/ioctl.h>
+#include <sys/types.h>
+#include <linux/fb.h> 
+
+#define  FB                      "/dev/fb0"
+#define  NO_OF_COLOR   2
+
+typedef unsigned char ubyte;
+ 
+unsigned short makepixel(ubyte r, ubyte g, ubyte b) {
+	return (unsigned short)( ((r>>3)<<11) | ((g>>2)<<5) | (b>>3) );
+}
+  
+int main(int argc, char** argv)
+{
+	int fd;
+	unsigned short pixel;
+	unsigned char color[NO_OF_COLOR];
+           
+	struct fb_var_screeninfo fbvar;
+	struct fb_fix_screeninfo fbfix;
+
+	fd = open(FB, O_RDWR);
+	if(fd < 0) {
+	printf("Error fbdev open\n");
+		return -1;
+	}
+
+	status = ioctl(fd, FBIOGET_FSCREENINFO, &fbfix);
+	status = ioctl(fd, FBIOGET_VSCREENINFO, &fbvar);
+
+	printf("x-resolution : %d\n", fbvar.xres);
+	printf("y-resolution : %d\n", fbvar.yres);
+	printf("x-resolution(virtual) : %d\n", fbvar.xres_virtual);
+	printf("y-resolution(virtual) : %d\n", fbvar.yres_virtual);
+	printf("bpp : %d\n", fbvar.bits_per_pixel);
+	printf("length of frame buffer memory : %d\n", fbfix.smem_len);
+
+	fbvar.bits_per_pixel = 16;
+	pfb = (unsigned short*)mmap(0, fbvar.xres_virtual* fbvar.yres_virtual
+			*NO_OF_COLOR, PROT_READ | PROT_WRITE, MMAP_SHARED, fd, 0);
+
+	for(j=0;j<fbvar.yres;j++) {
+		for(i=0;i<fbvar.xres/3;i++){
+			offset = (i + j*fbvar.xres)*NO_OF_COLOR;
+			lseek(fd, offset, SEEK_SET);
+			pixel = makepixel(0, 0, 255);
+			*(pfb + x + y * fbvar.xres) = pixel;	
+			//write(fd, &pixel,NO_OF_COLOR);
+		}
+	}
+
+	for(j=0;j<fbvar.yres;j++) {
+		for(i=fbvar.xres/3;i<(fbvar.xres/3)*2;i++){
+			offset = (i + j*fbvar.xres)*NO_OF_COLOR;
+			lseek(fd, offset, SEEK_SET);
+			pixel = makepixel(255, 255, 255);
+			*(pfb + x + y * fbvar.xres) = pixel;	
+			//write(fd, &pixel,NO_OF_COLOR);
+		}
+	}
+	
+	for(j=0;j<fbvar.yres_virtual;j++) {
+		for(i=(fbvar.xres/3)*2;i<fbvar.xres_virtual;i++){
+			offset = (i + j*fbvar.xres)*NO_OF_COLOR;
+			lseek(fd, offset, SEEK_SET);
+			pixel = makepixel(255, 0, 0);
+			*(pfb + x + y * fbvar.xres) = pixel;	
+			//write(fd, &pixel,NO_OF_COLOR);
+		}
+	}
+
+	printf("ERROR fbdev lseek \n");
+	munmap(pfb, fbvar.xres*fbvar.yres); 	
+	close(fd);
+	return 0;
+}

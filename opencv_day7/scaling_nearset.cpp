6ccbcf86a2a394467161747d0ff0381b6e955212
kimsungyeul <ksyeul92@gmail.com>
2022-12-16 14:57:36 +0900

    scaling_nearset

--- opencv_day7/scaling_nearset.cpp
+++ opencv_day7/scaling_nearset.cpp
@@ -19,19 +19,33 @@ void scaling(Mat img, Mat& dst, Size size)
 	}
 }
 
+void scaling_nearest(Mat img, Mat& dst, Size size)
+{
+	dst = Mat(size, CV_8U, Scalar(0));
+	double ratioY = (double)size.height / img.rows;
+	double ratioX = (double)size.width / img.cols;
+	
+	for(int i = 0; i < dst.rows; i++) {
+		for(int j = 0; j < dst.cols; j++) {
+			int x = (int)cvRound(j / ratioX);
+			int y = (int)cvRound(i / ratioY);
+			dst.at<uchar>(i, j) = img.at<uchar>(y, x);
+		}
+	}
+}
+
 int main()
 {
 	Mat image = imread("./lena.bmp", 0);
 	CV_Assert(image.data);
 
 	Mat dst1, dst2;
-	scaling(image, dst1, Size(150, 200));
-	scaling(image, dst2, Size(300, 400));
+	scaling(image, dst1, Size(300, 300));
+	scaling_nearest(image, dst2, Size(300, 300));
 
 	imshow("image", image),
-	imshow("dst1- 축소", dst1);
-	imshow("dst2- 확대", dst2);
-	resizeWindow("dst2- 축소", 200, 200);
+	imshow("dst1-순방향사상", dst1);
+	imshow("dst2-최근접 이웃보간", dst2);
 	waitKey();
 	return 1;
 }
